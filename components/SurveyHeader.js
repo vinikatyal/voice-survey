8d7f1656ecded1fb1124c5a719be2d11d2caf4db
Vini Katyal <vinikatyal90@gmail.com>
2022-01-27 17:09:03 +0530

    chore: formatting changes

--- components/SurveyHeader.js
+++ components/SurveyHeader.js
@@ -1,54 +1,59 @@
-import * as React from 'react'
+import * as React from "react";
 
-import Image from 'next/image'
-import styled from '@emotion/styled'
-import AppBar from '@mui/material/AppBar'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack'
-import Container from '@mui/material/Container'
-import { Button, Typography } from '@mui/material'
-import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight'
+import Image from "next/image";
+
+import AppBar from "@mui/material/AppBar";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+// icons
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
+
+import styled from "@emotion/styled";
 
 const AppBarCustom = styled(AppBar)(({}) => ({
-  backgroundColor: '#fff',
-  color: '#707070',
-}))
+  backgroundColor: "#fff",
+  color: "#707070",
+}));
 const HeaderContainer = styled(Container)(({}) => ({
-  color: '#707070',
-  display: 'flex',
-}))
+  color: "#707070",
+  display: "flex",
+}));
 
-const SurveyNameWrapped = styled('div')({
-  display: 'flex',
-  alignItems: 'center',
-  margin: '19px 0',
-  width: '40%',
-})
+const SurveyNameWrapped = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  margin: "19px 0",
+  width: "40%",
+});
 
-const Nav = styled('div')(({}) => ({
-  display: 'flex',
-  justifyContent: 'flex-start',
-  alignItems: 'center',
-  width: '60%',
-}))
+const Nav = styled("div")(({}) => ({
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  width: "60%",
+}));
 
-const NavItem = styled('div')(({ active }) => ({
-  color: active ? '#0a23fb' : '#707070',
-  marginLeft: '20px',
-  marginRight: '20px',
-  cursor: 'pointer',
-  '&:hover': {
-    color: '#0a23fb',
+const NavItem = styled("div")(({ active }) => ({
+  color: active ? "#0a23fb" : "#707070",
+  marginLeft: "20px",
+  marginRight: "20px",
+  cursor: "pointer",
+  "&:hover": {
+    color: "#0a23fb",
   },
-}))
+}));
 
 export default function Index() {
   return (
     <>
-      <AppBarCustom position='sticky'>
-        <HeaderContainer maxWidth='lg'>
+      <AppBarCustom position="sticky">
+        <HeaderContainer maxWidth="lg">
           <SurveyNameWrapped>
             <ArrowBackIcon />
-            <Typography variant='h4' ml={2} color='#00063e'>
+            <Typography variant="h4" ml={2} color="#00063e">
               Survey's Name Here
             </Typography>
           </SurveyNameWrapped>
@@ -62,5 +67,5 @@ export default function Index() {
         </HeaderContainer>
       </AppBarCustom>
     </>
-  )
+  );
 }
--- pages/survey/index.js
+++ pages/survey/index.js
@@ -1,61 +1,71 @@
-import * as React from 'react'
+import * as React from "react";
 
-import styled from '@emotion/styled'
-import Layout from '../../components/Layout'
-import SurveyHeader from '../../components/SurveyHeader'
-import QuestionMarkIcon from '@mui/icons-material/QuestionMark'
-import ColorLensIcon from '@mui/icons-material/ColorLens'
-import { Button, Container, TextField, Typography } from '@mui/material'
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 
-const SurveyHeadSection = styled('div')({
-  height: '60px',
-  backgroundColor: '#f5f8ff',
-})
+import Layout from "../../components/Layout";
+import SurveyHeader from "../../components/SurveyHeader";
+
+// icons
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
+import ColorLensIcon from "@mui/icons-material/ColorLens";
+
+import styled from "@emotion/styled";
+
+const SurveyHeadSection = styled("div")({
+  height: "60px",
+  backgroundColor: "#f5f8ff",
+});
 
 const HeaderContainer = styled(Container)(({}) => ({
-  color: '#707070',
-  display: 'flex',
-  justifyContent: 'space-between',
-}))
+  color: "#707070",
+  display: "flex",
+  justifyContent: "space-between",
+}));
 
-const SurveyHeadSelectionWrapper = styled('div')({
-  display: 'flex',
-  width: '30%',
-  height: '60px',
-  alignItems: 'center',
-})
+const SurveyHeadSelectionWrapper = styled("div")({
+  display: "flex",
+  width: "30%",
+  height: "60px",
+  alignItems: "center",
+});
 
-const TabButton = styled('div')(({ active }) => ({
-  background: active ? '#0A23FB' : '#fff',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  padding: '5px 10px',
-  borderRadius: '5px',
-  minWidth: '140px',
-  border: active ? 'none' : '1px solid #9a9cb5',
-  color: active ? 'white' : '#9a9cb5',
-  marginRight: '10px',
-  cursor: 'pointer',
-}))
+const TabButton = styled("div")(({ active }) => ({
+  background: active ? "#0A23FB" : "#fff",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: "5px 10px",
+  borderRadius: "5px",
+  minWidth: "140px",
+  border: active ? "none" : "1px solid #9a9cb5",
+  color: active ? "white" : "#9a9cb5",
+  marginRight: "10px",
+  cursor: "pointer",
+}));
 
-const Label = styled('span')({
-  color: '#707070',
-  fontSize: '16px',
-  fontWeight: '500',
-})
+const Label = styled("span")({
+  color: "#707070",
+  fontSize: "16px",
+  fontWeight: "500",
+});
 
 export default function Index() {
   return (
     <Layout>
       <SurveyHeader></SurveyHeader>
       <SurveyHeadSection>
-        <HeaderContainer maxWidth='lg'>
+        <HeaderContainer maxWidth="lg">
           <SurveyHeadSelectionWrapper>
             <TabButton active={true}>
               <QuestionMarkIcon sx={{ fontSize: 20 }} />
               <div>
-                <Typography variant='button' sx={{ textTransform: 'initial', fontSize: '16px' }}>
+                <Typography
+                  variant="button"
+                  sx={{ textTransform: "initial", fontSize: "16px" }}
+                >
                   Questions
                 </Typography>
               </div>
@@ -63,24 +73,35 @@ export default function Index() {
             <TabButton>
               <ColorLensIcon mr={10} sx={{ fontSize: 20 }} />
               <div>
-                <Typography variant='button' sx={{ textTransform: 'initial', fontSize: '16px' }}>
+                <Typography
+                  variant="button"
+                  sx={{ textTransform: "initial", fontSize: "16px" }}
+                >
                   Design
                 </Typography>
               </div>
             </TabButton>
           </SurveyHeadSelectionWrapper>
-          <Button variant='outlined' sx={{ margin: '10px 0' }}>
-            <Typography variant='button' sx={{ textTransform: 'initial', fontSize: '16px' }}>
+          <Button variant="outlined" sx={{ margin: "10px 0" }}>
+            <Typography
+              variant="button"
+              sx={{ textTransform: "initial", fontSize: "16px" }}
+            >
               Preview
             </Typography>
           </Button>
         </HeaderContainer>
 
-        <Container maxWidth='lg' sx={{ marginTop: '30px' }}>
+        <Container maxWidth="lg" sx={{ marginTop: "30px" }}>
           <Label>Welcome Text</Label>
-          <TextField id='outlined-basic' placeholder='Enter your welcome text here' fullWidth variant='outlined' />
+          <TextField
+            id="outlined-basic"
+            placeholder="Enter your welcome text here"
+            fullWidth
+            variant="outlined"
+          />
         </Container>
       </SurveyHeadSection>
     </Layout>
-  )
+  );
 }
