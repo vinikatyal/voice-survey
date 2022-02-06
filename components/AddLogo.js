0511dd9a65500245dbc9d09eb0ccbde3b92e60e7
Shaik Sadath Basha <sadath.sa@gmail.com>
2022-02-07 02:10:30 +0530

    feat: Add logo and preview image feature

--- components/AddLogo.js
+++ components/AddLogo.js
@@ -4,8 +4,10 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 import styled from "@emotion/styled";
+import Image from "next/image";
 
 const LogoInputSection = styled(Box)({
   width: "140px",
@@ -18,7 +20,7 @@ const LogoInputSection = styled(Box)({
   flexDirection: "column",
   justifyContent: "space-around",
   alignItems: "center",
-  padding: "25px",
+  padding: "5px",
 });
 
 const LogoAddButton = styled(Fab)({
@@ -26,15 +28,96 @@ const LogoAddButton = styled(Fab)({
   width: "36px",
 });
 
+const Input = styled("input")({
+  display: "none",
+});
+
+const StyledTypography = styled(Typography)({
+  textDecoration: "underline",
+  cursor: "pointer",
+  transform: "scale(1)",
+  transition: "0.2s ease",
+  "&:active": {
+    transform: "scale(0.95)",
+  },
+});
+
 export default function AddLogo() {
+  const addLogoRef = React.useRef();
+  const logoInp = React.useRef();
+  const [imageSrc, setImageSrc] = React.useState("");
+
+  const handleInputChange = (event) => {
+    const reader = new FileReader(event.target.files[0]);
+    reader.onload = async () => {
+      addLogoRef.current.style.display = "none";
+      setImageSrc(reader.result);
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  };
+
+  const changeLogo = () => {
+    logoInp.current.click();
+  };
+  const removeLogo = () => {
+    addLogoRef.current.style.display = "flex";
+    setImageSrc("");
+  };
+
   return (
-    <LogoInputSection>
-      <LogoAddButton color="primary" variant="contained">
-        <Typography fontSize={28}>+</Typography>
-      </LogoAddButton>
-      <Typography fontSize={18} color={"#0a23fb"}>
-        Add Logo
-      </Typography>
-    </LogoInputSection>
+    <Grid container justifyContent="center">
+      <LogoInputSection>
+        <Grid
+          container
+          justifyContent="center"
+          direction="column"
+          alignItems="center"
+          ref={addLogoRef}
+        >
+          <label htmlFor="contained-button-file">
+            <Input
+              ref={logoInp}
+              accept="image/*"
+              id="contained-button-file"
+              type="file"
+              onChange={handleInputChange}
+            />
+            <LogoAddButton color="primary" variant="contained" component="span">
+              <Typography fontSize={28}>+</Typography>
+            </LogoAddButton>
+          </label>
+          <Typography fontSize={18} color={"#0a23fb"}>
+            Add Logo
+          </Typography>
+        </Grid>
+        {imageSrc && (
+          <Grid container>
+            <Image height={140} width={140} src={imageSrc} />
+          </Grid>
+        )}
+      </LogoInputSection>
+      {imageSrc && (
+        <Grid container justifyContent="center" spacing={1}>
+          <Grid item>
+            <StyledTypography
+              onClick={changeLogo}
+              variant="subtitle2"
+              color="#0a23fb"
+            >
+              Change Logo
+            </StyledTypography>
+          </Grid>
+          <Grid item>
+            <StyledTypography
+              onClick={removeLogo}
+              variant="subtitle2"
+              color="#fd574f"
+            >
+              Remove Logo
+            </StyledTypography>
+          </Grid>
+        </Grid>
+      )}
+    </Grid>
   );
 }
