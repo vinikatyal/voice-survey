2e42824006a162fc5abf019ebde776de5d98f7c4
Vini Katyal <vinikatyal90@gmail.com>
2022-02-26 11:33:34 +0530

    chore: some more ui issues fix them

--- pages/api/survey/[id].js
+++ pages/api/survey/[id].js
@@ -4,5 +4,10 @@ export default async function handler(req, res) {
   const resData = await fetch(`${BASE_URL}/get_survey_details_link/${id}`).then(
     (response) => response.json()
   );
-  return res.status(200).json(resData);
+
+  if (resData.code && resData.code === 200) {
+    return res.status(200).json(resData);
+  } else {
+    return res.status(500).json({ error: "There was an error" });
+  }
 }
