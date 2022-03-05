fac35f6c17fdfd1dfdfba9be0d3464bb91357b06
Shaik Sadath Basha <sadath.sa@gmail.com>
2022-03-06 02:18:04 +0530

    feat: initial report statics design

--- pages/survey/report/answers.js
+++ pages/survey/report/answers.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { useSurvey } from "../../context/SurveyState";
+import { useSurvey } from "../../../context/SurveyState";
 
 import Container from "@mui/material/Container";
 
-import Layout from "../../components/Layout";
-import SurveyHeader from "../../components/survey/SurveyHeader";
-import UserSurveyQuesAnsSection from "../../components/survey/UserSurveyQuesAnsSection";
+import Layout from "../../../components/Layout";
+import SurveyHeader from "../../../components/survey/SurveyHeader";
+import UserSurveyQuesAnsSection from "../../../components/survey/UserSurveyQuesAnsSection";
 
 import styled from "@emotion/styled";
 
@@ -14,9 +14,11 @@ export default function answers() {
   const survey = useSurvey();
   return (
     <Layout>
-      <SurveyHeader headerTitle={survey.surveyTitle}
+      <SurveyHeader
+        headerTitle={survey.surveyTitle}
         backRoute="/survey/create"
-        currentTab="ANSWERS">
+        currentTab="REPORT"
+      >
         <Container maxWidth="lg">
           <UserSurveyQuesAnsSection />
         </Container>
